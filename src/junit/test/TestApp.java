4a9753115d8ae07956b81b484cb7b3c9c30d9096
YY <649775655@qq.com>
2020-07-09 20:07:54 +0800

    SSM-First

---
+++ src/junit/test/TestApp.java
@@ -0,0 +1,29 @@
+package junit.test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:beans.xml"})
+public class TestApp {
+
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void test01() throws SQLException {
+		
+		Connection conn = dataSource.getConnection();
+		
+		System.out.println(conn);
+		
+	}
+}
